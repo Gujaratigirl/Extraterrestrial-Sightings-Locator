6f41e88ff8da1139debf5275d51080df9341b3ec
HemaVyas <Hema@hkls-mbp.lan>
2021-06-18 10:18:32 -0400

    All columns pull

--- UFO-level_1/app.js
+++ UFO-level_1/app.js
@@ -30,19 +30,42 @@ function runFilter() {
     
     var dateArray = filteredData.map(row => row.datetime);
     // console.log(dateArray);
-    var cityArray = filteredData.map(row => row.city);
-
     //Select column placeholder for date
     var dateInfo = d3.select(".table-head-date");
     //remove any children from the list -CLEAR old data
     dateInfo.html("");
     dateArray.forEach(row => dateInfo.append("li").text(row));
 
-
+    var cityArray = filteredData.map(row => row.city);
     var cityInfo = d3.select(".table-head-city");
     cityInfo.html("");
-
     cityArray.forEach(row => cityInfo.append("li").text(row));
+
+    var stateArray = filteredData.map(row => row.state);
+    var stateInfo = d3.select(".table-head-state");
+    stateInfo.html("");
+    stateArray.forEach(row => stateInfo.append("li").text(row));
+
+    var countryArray = filteredData.map(row => row.country);
+    var countryInfo = d3.select(".table-head-country");
+    countryInfo.html("");
+    countryArray.forEach(row => countryInfo.append("li").text(row));
+
+    var shapeArray = filteredData.map(row => row.shape);
+    var shapeInfo = d3.select(".table-head-shape");
+    shapeInfo.html("");
+    shapeArray.forEach(row => shapeInfo.append("li").text(row));
+
+    var durationArray = filteredData.map(row => row.durationMinutes);
+    var durationInfo = d3.select(".table-head-duration");
+    durationInfo.html("");
+    durationArray.forEach(row => durationInfo.append("li").text(row));
+
+    var commentsArray = filteredData.map(row => row.comments);
+    var commentsInfo = d3.select(".table-head-comments");
+    commentsInfo.html("");
+    commentsArray.forEach(row => commentsInfo.append("li").text(row));
+
     console.log("End Code");
 };
   
